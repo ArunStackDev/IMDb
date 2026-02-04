befca8101fd57fcc34a576e3a81a0d2ed4deaa24
ArunStackDev <thearunkumar.dev@gmail.com>
2026-02-04 20:08:19 +0530

    api integration in movies card done

--- src/components/MovieCart.jsx
+++ src/components/MovieCart.jsx
@@ -1,15 +1,17 @@
 import React from "react";
 
-function MovieCart() {
+function MovieCart({movieRatings, moviePoster}) {
+  
+      let formattedRatings = movieRatings.toFixed(1);
   return (
     <div className="flex items-center justify-center h-70 w-45 rounded-lg m-2 bg-cover bg-center hover:scale-110 transition-transform duration-300"
     
       style={{
         backgroundImage:
-          "url('https://preview.redd.it/a-few-of-the-prettiest-posters-from-recent-malayalam-movies-v0-l2zcca6lbre91.jpg?width=493&format=pjpg&auto=webp&s=07e11aac02f8968682e12a34df534fb1bc671f7f')",
+          `url('https://image.tmdb.org/t/p/w500${moviePoster}')`,
       }}
     >
-      <h5 className="text-black bg-indigo-100 w-full text-center">Movie name</h5>
+      <h5 className="text-black bg-indigo-100 w-full text-center">Ratings : {formattedRatings}⭐</h5>
     </div>
   );
 }
--- src/components/Movies.jsx
+++ src/components/Movies.jsx
@@ -1,14 +1,24 @@
 import React, { useState, useEffect } from "react";
 import Banner from "./Banner.jsx";
 import MovieCart from "./MovieCart.jsx";
-import axios from 'axios'
+import axios from "axios";
+import { data } from "react-router-dom";
 // https://api.themoviedb.org/3/movie/now_playing?language=en-US&page=1
 
 function Movies() {
   const [movies, setMovies] = useState([]);
-  useEffect(()=>{
-   const data = axios.get(`https://api.themoviedb.org/3/movie/now_playing?language=en-US&page=1`)
-  },[])
+  useEffect(() => {
+    async function fetchMovies() {
+      const response = await axios.get(
+        `https://api.themoviedb.org/3/movie/now_playing?api_key=48498188740fff63e2567b71fb163e44&language=en-US&page=1`,
+      );
+
+      setMovies(response.data.results);
+    }
+    fetchMovies();
+  }, []);
+
+  console.log(movies);
 
   return (
     <div>
@@ -18,24 +28,9 @@ function Movies() {
       </h3>
 
       <div className="flex flex-wrap justify-center gap-20">
-        <MovieCart />
-        <MovieCart />
-        <MovieCart />
-        <MovieCart />
-        <MovieCart />
-        <MovieCart />
-        <MovieCart />
-        <MovieCart />
-        <MovieCart />
-        <MovieCart />
-        <MovieCart />
-        <MovieCart />
-        <MovieCart />
-        <MovieCart />
-        <MovieCart />
-        <MovieCart />
-        <MovieCart />
-        <MovieCart />
+        {movies.map((movie) => (
+          <MovieCart movieRatings={movie.vote_average} moviePoster={movie.poster_path} />
+        ))}
       </div>
     </div>
   );
